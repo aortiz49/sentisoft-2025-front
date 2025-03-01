c43b19891e12a65d46276a05a64a9f89bce5be05
Andy Ortiz <andyortiz93@gmail.com>
2025-03-01 16:06:30 -0500

    use card to render info

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,15 @@
 import { Snippet } from '@heroui/snippet';
 import { Button } from '@heroui/button';
 import { useState } from 'react';
+import { Card, CardBody } from '@heroui/card';
 
 import { title, subtitle } from '@/components/primitives';
 import DefaultLayout from '@/layouts/default';
-
 export default function IndexPage() {
   const [isLoading, setIsLoading] = useState(false);
+
+  const [renderOption, setRenderOption] = useState(true);
+
   const [pokemonData, setPokemonData] = useState<{
     name: string;
     id: number;
@@ -21,6 +24,7 @@ export default function IndexPage() {
       setTimeout(() => {
         setPokemonData({ name: data.name, id: data.id });
         setIsLoading(false);
+        setRenderOption(false);
       }, 1000);
     } catch (error) {
       console.error('Error fetching Pokemon data:', error);
@@ -41,28 +45,49 @@ export default function IndexPage() {
             sessions.
           </div>
         </div>
-        <div className="mt-8">
-          <Snippet hideCopyButton hideSymbol variant="bordered">
-            <span>
-              Start your first interview{' '}
-              <Button
-                className="bg-gradient-to-tr from-[#FF1CF7] to-[#b249f8] text-white shadow-lg"
-                isLoading={isLoading}
-                radius="full"
-                variant="shadow"
-                onPress={handleClick}
-              >
-                <p className="leading-none">Start</p>
-              </Button>
-            </span>
-          </Snippet>
-          {pokemonData && (
-            <div className="mt-4 text-center">
-              <p>Name: {pokemonData.name}</p>
-              <p>ID: {pokemonData.id}</p>
-            </div>
-          )}
-        </div>
+        {renderOption && (
+          <div className="mt-8">
+            <Snippet hideCopyButton hideSymbol variant="bordered">
+              <span>
+                Start your first interview{' '}
+                <Button
+                  className="bg-gradient-to-tr from-[#FF1CF7] to-[#b249f8] text-white shadow-lg"
+                  isLoading={isLoading}
+                  radius="full"
+                  variant="shadow"
+                  onPress={handleClick}
+                >
+                  <p className="leading-none">Start</p>
+                </Button>
+              </span>
+            </Snippet>
+          </div>
+        )}
+        {pokemonData && (
+          <Card
+            isBlurred
+            className="border-none bg-background/60 dark:bg-default-100/50 max-w-[610px]"
+            shadow="sm"
+          >
+            <CardBody>
+              <div className="grid grid-cols-6 md:grid-cols-12 gap-6 md:gap-4 items-center justify-center">
+                <div className="flex flex-col col-span-6 md:col-span-8">
+                  <div className="flex justify-between items-start">
+                    <div className="flex flex-col gap-0">
+                      <h3 className="font-semibold text-foreground/90">
+                        Daily Mix
+                      </h3>
+                      <p className="text-small text-foreground/80">12 Tracks</p>
+                      <h1 className="text-large font-medium mt-2">
+                        Frontend Radio
+                      </h1>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </CardBody>
+          </Card>
+        )}
       </section>
     </DefaultLayout>
   );
