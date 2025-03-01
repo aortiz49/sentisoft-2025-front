f63fa523174ec185898354a8623ff95baa153f15
Andy Ortiz <andyortiz93@gmail.com>
2025-03-01 12:18:54 -0500

    adds initial HeroUI set

---
+++ vite.config.ts
@@ -0,0 +1,24 @@
+import react from '@vitejs/plugin-react';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import { defineConfig, loadEnv } from 'vite';
+import path from 'path';
+
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd(), '');
+
+    const host = env.VITE_HOST || 'localhost';
+    const port = Number(env.VITE_PORT) || 5173;
+
+    return {
+        plugins: [react(), tsconfigPaths()],
+        resolve: {
+            alias: {
+                '#': path.resolve(__dirname), // Resolves # to the root of the project
+            },
+        },
+        server: {
+            host,
+            port,
+        },
+    };
+});
